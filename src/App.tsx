72e0e9c59064851ec02cbd3ae067d1d0e9fe5275
desaiHarsh08 <desaiharsh183@gmail.com>
2025-02-11 13:07:50 +0530

    added taskboard

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import SearchTask from "./pages/protected/SearchTask";
 import Report from "./pages/protected/Report";
 import ActivityLogs from "./pages/protected/ActivityLogs";
 import { AuthProvider } from "./providers/AuthProvider";
-import Home from "./pages/protected/Home";
+
 // import Home from "./pages/protected/Home";
 
 const router = createBrowserRouter([
@@ -32,8 +32,8 @@ const router = createBrowserRouter([
       </AuthProvider>
     ),
     children: [
-        { path: "", element: <Home /> },
-    //   { path: "", element: <TaskBoard /> },
+        // { path: "", element: <Home /> },
+      { path: "", element: <TaskBoard /> },
         { path: "taskboard", element: <TaskBoard /> },
       { path: "activity-logs", element: <ActivityLogs /> },
       {
